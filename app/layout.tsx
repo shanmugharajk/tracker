a9c142187b0ee5b0897efadcc9fc35d43296e228
shanmugharajk <shanmugharaj.k@outlook.com>
2026-04-05 19:38:15 -0400

    chore: setup shadcn

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,16 +1,16 @@
 import type { Metadata } from 'next';
 import { Geist, Geist_Mono } from 'next/font/google';
 
+import { cn } from '~/lib/cn';
+
 import '~/app/styles/globals.css';
+import { ThemeProvider } from '~/components/theme-provider';
 
-const geistSans = Geist({
-  variable: '--font-geist-sans',
-  subsets: ['latin'],
-});
+const geist = Geist({ subsets: ['latin'], variable: '--font-sans' });
 
-const geistMono = Geist_Mono({
-  variable: '--font-geist-mono',
+const fontMono = Geist_Mono({
   subsets: ['latin'],
+  variable: '--font-mono',
 });
 
 export const metadata: Metadata = {
@@ -26,9 +26,17 @@ export default function RootLayout({
   return (
     <html
       lang="en"
-      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+      suppressHydrationWarning
+      className={cn(
+        'antialiased',
+        fontMono.variable,
+        'font-sans',
+        geist.variable
+      )}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body>
+        <ThemeProvider>{children}</ThemeProvider>
+      </body>
     </html>
   );
 }
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,7 @@
 export default function Home() {
-  return <div>Tracker</div>;
+  return (
+    <div>
+      <h1>Tracker</h1>
+    </div>
+  );
 }
